a9ff06ac0c03ab6ead33268ff60543604c3eabcb
Nicolas <nfilipov@cern.ch>
2016-01-11 16:08:57 +0100

    electron part of the skim: OK

--- Skimming/TInputOutputTree.C
+++ Skimming/TInputOutputTree.C
@@ -15,6 +15,7 @@ TInputOutputTree::~TInputOutputTree()
 }
 
 void TInputOutputTree::InitInput(TTree *tree){
+  //global
   treeLeaf.nVtx =0;
   treeLeaf.nVtx=0;		
   treeLeaf.run=0;		
@@ -33,11 +34,110 @@ void TInputOutputTree::InitInput(TTree *tree){
   treeLeaf.HLTEleMuXIsPrescaled=0;
   treeLeaf.HLTPhoIsPrescaled=0;
   treeLeaf.HLTJetIsPrescaled=0;
+  //electron
+  treeLeaf.nEle=0;
+  treeLeaf.eleCharge=0;
+  treeLeaf.eleChargeConsistent=0;
+  treeLeaf.eleEn=0;
+  treeLeaf.eleSCEn=0;
+  /*  treeLeaf.eleESEn=0; */
+  /*  treeLeaf.eleESEnP1=0; */
+  /*  treeLeaf.eleESEnP2=0; */
+  /*  treeLeaf.eleESEnP1Raw=0; */
+  /*  treeLeaf.eleESEnP2Raw=0; */
+  treeLeaf.eleD0=0;
+  treeLeaf.eleDz=0;
+  treeLeaf.elePt=0;
+  treeLeaf.eleEta=0;
+  treeLeaf.elePhi=0;
+  treeLeaf.eleR9=0;
+  treeLeaf.eleSCEta=0;
+  treeLeaf.eleSCPhi=0;
+  treeLeaf.eleSCRawEn=0;
+  /*   treeLeaf.eleSCEtaWidth=0; */
+  /*   treeLeaf.eleSCPhiWidth=0; */
+  treeLeaf.eleHoverE=0;
+  treeLeaf.eleEoverP=0;
+  /*  treeLeaf.eleEoverPout=0; */
+  /*  treeLeaf.eleEoverPInv=0; */
+  treeLeaf.eleBrem=0;
+  /*  treeLeaf.eledEtaAtVtx=0; */
+  /*  treeLeaf.eledPhiAtVtx=0; */
+  /*  treeLeaf.eledEtaAtCalo=0; */
+  treeLeaf.eleSigmaIEtaIEta=0;
+  treeLeaf.eleSigmaIEtaIPhi=0;
+  treeLeaf.eleSigmaIPhiIPhi=0;
+  treeLeaf.eleSigmaIEtaIEtaFull5x5=0;
+  treeLeaf.eleSigmaIPhiIPhiFull5x5=0;
+  treeLeaf.eleConvVeto=0;
+  treeLeaf.eleMissHits=0;
+  treeLeaf.eleESEffSigmaRR=0;
+  treeLeaf.elePFChIso=0;
+  treeLeaf.elePFPhoIso=0;
+  treeLeaf.elePFNeuIso=0;
+  treeLeaf.elePFPUIso=0;
+  treeLeaf.elePFClusEcalIso=0;
+  treeLeaf.elePFClusHcalIso=0;
+  /*  treeLeaf.eleIDMVANonTrg=0; */
+  /*  treeLeaf.eleIDMVATrg=0; */
+  /*  treeLeaf.eledEtaseedAtVtx=0; */
+  /*  treeLeaf.eleE1x5=0; */
+  /*  treeLeaf.eleE2x5=0; */
+  /*  treeLeaf.eleE5x5=0; */
+  /*  treeLeaf.eleE1x5Full5x5=0; */
+  /*  treeLeaf.eleE2x5Full5x5=0; */
+  /*  treeLeaf.eleE5x5Full5x5=0; */
+  /*  treeLeaf.eleR9Full5x5=0; */
+  /*  treeLeaf.eleEcalDrivenSeed=0; */
+  /*  treeLeaf.eleDr03EcalRecHitSumEt=0; */
+  /*  treeLeaf.eleDr03HcalDepth1TowerSumEt=0; */
+  /*  treeLeaf.eleDr03HcalDepth2TowerSumEt=0; */
+  /*  treeLeaf.eleDr03HcalTowerSumEt=0; */
+  /*  treeLeaf.eleDr03TkSumPt=0; */
+  /*  treeLeaf.elecaloEnergy=0; */
+  /*  treeLeaf.eleTrkdxy=0; */
+  /*  treeLeaf.eleKFHits=0; */
+  /*  treeLeaf.eleKFChi2=0; */
+  /*  treeLeaf.eleGSFChi2=0; */
+  /*  treeLeaf.eleGSFPt=0; */
+  /*  treeLeaf.eleGSFEta=0; */
+  /*  treeLeaf.eleGSFPhi=0; */
+  /*  treeLeaf.eleGSFCharge=0; */
+  /*  treeLeaf.eleGSFHits=0; */
+  /*  treeLeaf.eleGSFMissHits=0; */
+  /*  treeLeaf.eleGSFNHitsMax=0; */
+  /*  treeLeaf.eleGSFVtxProb=0; */
+  /*  treeLeaf.eleGSFlxyPV=0; */
+  /*  treeLeaf.eleGSFlxyBS=0; */
+  /*  treeLeaf.eleBCEn=0; */
+  /*  treeLeaf.eleBCEta=0; */
+  /*  treeLeaf.eleBCPhi=0; */
+  /*  treeLeaf.eleBCS25=0; */
+  /*  treeLeaf.eleBCS15=0; */
+  /*  treeLeaf.eleBCSieie=0; */
+  /*  treeLeaf.eleBCSieip=0; */
+  /*  treeLeaf.eleBCSipip=0; */
+  /*  treeLeaf.eleESEnEta=0; */
+  /*  treeLeaf.eleESEnPhi=0; */
+  /*  treeLeaf.eleESEnZ=0; */
+  /*  treeLeaf.eleESEnP=0; */
+  /*  treeLeaf.eleESEnX=0; */
+  /*  treeLeaf.eleESEnY=0; */
+  /*  treeLeaf.eleESEnS=0; */
+  /*  treeLeaf.eleESEnE=0; */
+  /*  treeLeaf.nGSFTrk=0; */
+  /*  treeLeaf.gsfPt=0; */
+  /*  treeLeaf.gsfEta=0; */
+  /*  treeLeaf.gsfPhi=0; */
+  treeLeaf.eleFiredTrgs=0;
+  treeLeaf.eleIDbit=0; 
+
 
   if (!tree) return;
   fChain = tree;
   fCurrent = -1;
   fChain->SetMakeClass(1);
+  //global event
   fChain->SetBranchAddress("nVtx", &treeLeaf.nVtx, &b_nVtx);
   fChain->SetBranchAddress("run", &treeLeaf.run, &b_run);
   fChain->SetBranchAddress("event", &treeLeaf.event, &b_event);
@@ -55,10 +155,45 @@ void TInputOutputTree::InitInput(TTree *tree){
   fChain->SetBranchAddress("HLTEleMuXIsPrescaled", &treeLeaf.HLTEleMuXIsPrescaled, &b_HLTEleMuXIsPrescaled);
   fChain->SetBranchAddress("HLTPhoIsPrescaled", &treeLeaf.HLTPhoIsPrescaled, &b_HLTPhoIsPrescaled);
   fChain->SetBranchAddress("HLTJetIsPrescaled", &treeLeaf.HLTJetIsPrescaled, &b_HLTJetIsPrescaled);
+  //electron channel
+  fChain->SetBranchAddress("nEle",                    &treeLeaf.nEle,                &b_nEle	     );
+  fChain->SetBranchAddress("eleCharge",               &treeLeaf.eleCharge,           &b_eleCharge            );
+  fChain->SetBranchAddress("eleChargeConsistent",     &treeLeaf.eleChargeConsistent, &b_eleChargeConsistent    );
+  fChain->SetBranchAddress("eleEn",                   &treeLeaf.eleEn,               &b_eleEn   );
+  fChain->SetBranchAddress("eleSCEn",                 &treeLeaf.eleSCEn,             &b_eleSCEn );
+  fChain->SetBranchAddress("eleD0",                   &treeLeaf.eleD0 , &b_eleD0);
+  fChain->SetBranchAddress("eleDz",                   &treeLeaf.eleDz , &b_eleDz);
+  fChain->SetBranchAddress("elePt", &treeLeaf.elePt, &b_elePt);
+  fChain->SetBranchAddress("eleEta", &treeLeaf.eleEta, &b_eleEta);
+  fChain->SetBranchAddress("elePhi", &treeLeaf.elePhi, &b_elePhi);
+  fChain->SetBranchAddress("eleR9", &treeLeaf.eleR9, &b_eleR9);
+  fChain->SetBranchAddress("eleSCEta", &treeLeaf.eleSCEta, &b_eleSCEta);
+  fChain->SetBranchAddress("eleSCPhi", &treeLeaf.eleSCPhi, &b_eleSCPhi);
+  fChain->SetBranchAddress("eleSCRawEn", &treeLeaf.eleSCRawEn, &b_eleSCRawEn);
+  fChain->SetBranchAddress("eleHoverE",               &treeLeaf.eleHoverE, &b_eleHoverE);
+  fChain->SetBranchAddress("eleEoverP",               &treeLeaf.eleEoverP, &b_eleEoverP);
+  fChain->SetBranchAddress("eleBrem",                 &treeLeaf.eleBrem, &b_eleBrem);
+  fChain->SetBranchAddress("eleSigmaIEtaIEta",        &treeLeaf.eleSigmaIEtaIEta, &b_eleSigmaIEtaIEta);
+  fChain->SetBranchAddress("eleSigmaIEtaIPhi",        &treeLeaf.eleSigmaIEtaIPhi, &b_eleSigmaIEtaIPhi);
+  fChain->SetBranchAddress("eleSigmaIPhiIPhi",        &treeLeaf.eleSigmaIPhiIPhi, &b_eleSigmaIPhiIPhi);
+  fChain->SetBranchAddress("eleSigmaIEtaIEtaFull5x5", &treeLeaf.eleSigmaIEtaIEtaFull5x5, &b_eleSigmaIEtaIEtaFull5x5);
+  fChain->SetBranchAddress("eleSigmaIPhiIPhiFull5x5", &treeLeaf.eleSigmaIPhiIPhiFull5x5, &b_eleSigmaIPhiIPhiFull5x5);
+  fChain->SetBranchAddress("eleConvVeto",             &treeLeaf.eleConvVeto, &b_eleConvVeto);
+  fChain->SetBranchAddress("eleMissHits",             &treeLeaf.eleMissHits, &b_eleMissHits);
+  fChain->SetBranchAddress("eleESEffSigmaRR",         &treeLeaf.eleESEffSigmaRR, &b_eleESEffSigmaRR);
+  fChain->SetBranchAddress("elePFChIso",              &treeLeaf.elePFChIso, &b_elePFChIso);
+  fChain->SetBranchAddress("elePFPhoIso",             &treeLeaf.elePFPhoIso, &b_elePFPhoIso);
+  fChain->SetBranchAddress("elePFNeuIso",             &treeLeaf.elePFNeuIso, &b_elePFNeuIso);
+  fChain->SetBranchAddress("elePFPUIso",              &treeLeaf.elePFPUIso, &b_elePFPUIso);
+  fChain->SetBranchAddress("elePFClusEcalIso",        &treeLeaf.elePFClusEcalIso, &b_elePFClusEcalIso);
+  fChain->SetBranchAddress("elePFClusHcalIso",        &treeLeaf.elePFClusHcalIso, &b_elePFClusHcalIso);
+  fChain->SetBranchAddress("eleFiredTrgs",                &treeLeaf.eleFiredTrgs, &b_eleFiredTrgs);
+  fChain->SetBranchAddress("eleIDbit",  &treeLeaf.eleIDbit, &b_eleIDbit);
 }
 
 void TInputOutputTree::InitOutput(TTree* outputTree)
 {
+  //global event (keep all, as general rule)
   outputTree->Branch("nVtx",                 &treeLeaf.nVtx,       "nVtx/I");
   outputTree->Branch("run",                  &treeLeaf.run,        "run/I");
   outputTree->Branch("event",                &treeLeaf.event,      "event/L");
@@ -76,5 +211,104 @@ void TInputOutputTree::InitOutput(TTree* outputTree)
   outputTree->Branch("HLTEleMuXIsPrescaled", &treeLeaf.HLTEleMuXIsPrescaled);
   outputTree->Branch("HLTPhoIsPrescaled",    &treeLeaf.HLTPhoIsPrescaled);
   outputTree->Branch("HLTJetIsPrescaled",    &treeLeaf.HLTJetIsPrescaled);
+  // electrons (only kinematics, trigger, for now)
+  outputTree->Branch("nEle",                    &treeLeaf.nEle,                    "nEle/I");
+  outputTree->Branch("eleCharge",               &treeLeaf.eleCharge,               "eleCharge/I");
+  outputTree->Branch("eleChargeConsistent",     &treeLeaf.eleChargeConsistent,     "eleChargeConsistent/I");
+  outputTree->Branch("eleEn",                   &treeLeaf.eleEn,                   "eleEn/F");
+  outputTree->Branch("eleSCEn",                 &treeLeaf.eleSCEn,                 "eleSCEn/F");
+  // outputTree->Branch("eleESEn",                 &treeLeaf.eleESEn);
+  // outputTree->Branch("eleESEnP1",               &treeLeaf.eleESEnP1);
+  // outputTree->Branch("eleESEnP2",               &treeLeaf.eleESEnP2);
+  outputTree->Branch("eleD0",                   &treeLeaf.eleD0, "eleD0/F");
+  outputTree->Branch("eleDz",                   &treeLeaf.eleDz, "eleDz/F");
+  outputTree->Branch("elePt",                   &treeLeaf.elePt, "elePt/F");
+  outputTree->Branch("eleEta",                  &treeLeaf.eleEta, "eleEta/F");
+  outputTree->Branch("elePhi",                  &treeLeaf.elePhi, "elePhi/F");
+  outputTree->Branch("eleR9",                   &treeLeaf.eleR9, "eleR9/F");
+  outputTree->Branch("eleSCEta",                &treeLeaf.eleSCEta, "eleSCEta/F");
+  outputTree->Branch("eleSCPhi",                &treeLeaf.eleSCPhi, "eleSCPhi/F");
+  outputTree->Branch("eleSCRawEn",              &treeLeaf.eleSCRawEn, "eleSCRawEn/F");
+  // outputTree->Branch("eleSCEtaWidth",           &treeLeaf.eleSCEtaWidth);
+  // outputTree->Branch("eleSCPhiWidth",           &treeLeaf.eleSCPhiWidth);
+  outputTree->Branch("eleHoverE",               &treeLeaf.eleHoverE, "eleHoverE/F");
+  outputTree->Branch("eleEoverP",               &treeLeaf.eleEoverP, "eleEoverP/F");
+  // outputTree->Branch("eleEoverPout",            &treeLeaf.eleEoverPout);
+  // outputTree->Branch("eleEoverPInv",            &treeLeaf.eleEoverPInv);
+  outputTree->Branch("eleBrem",                 &treeLeaf.eleBrem, "eleBrem/F");
+  // outputTree->Branch("eledEtaAtVtx",            &treeLeaf.eledEtaAtVtx);
+  // outputTree->Branch("eledPhiAtVtx",            &treeLeaf.eledPhiAtVtx);
+  // outputTree->Branch("eledEtaAtCalo",           &treeLeaf.eledEtaAtCalo);
+  outputTree->Branch("eleSigmaIEtaIEta",        &treeLeaf.eleSigmaIEtaIEta, "eleSigmaIEtaIEta/F");
+  outputTree->Branch("eleSigmaIEtaIPhi",        &treeLeaf.eleSigmaIEtaIPhi, "eleSigmaIEtaIPhi/F");
+  outputTree->Branch("eleSigmaIPhiIPhi",        &treeLeaf.eleSigmaIPhiIPhi, "eleSigmaIPhiIPhi/F");
+  outputTree->Branch("eleSigmaIEtaIEtaFull5x5", &treeLeaf.eleSigmaIEtaIEtaFull5x5, "eleSigmaIEtaIEtaFull5x5/F");
+  outputTree->Branch("eleSigmaIPhiIPhiFull5x5", &treeLeaf.eleSigmaIPhiIPhiFull5x5, "eleSigmaIPhiIPhi5x5/F");
+  outputTree->Branch("eleConvVeto",             &treeLeaf.eleConvVeto, "eleConvVeto/I");
+  outputTree->Branch("eleMissHits",             &treeLeaf.eleMissHits, "eleMissHits/I");
+  outputTree->Branch("eleESEffSigmaRR",         &treeLeaf.eleESEffSigmaRR, "eleESEffSigmaRR/F");
+  outputTree->Branch("elePFChIso",              &treeLeaf.elePFChIso, "elePFChIso/F");
+  outputTree->Branch("elePFPhoIso",             &treeLeaf.elePFPhoIso, "elePFPhoIso/F");
+  outputTree->Branch("elePFNeuIso",             &treeLeaf.elePFNeuIso, "elePFNeuIso/F");
+  outputTree->Branch("elePFPUIso",              &treeLeaf.elePFPUIso, "elePFPUIso/F");
+  outputTree->Branch("elePFClusEcalIso",        &treeLeaf.elePFClusEcalIso, "elePFClusEcalIso/F");
+  outputTree->Branch("elePFClusHcalIso",        &treeLeaf.elePFClusHcalIso, "elePFClusHCalIso/F");
+  // outputTree->Branch("eleIDMVANonTrg",          &treeLeaf.eleIDMVANonTrg);
+  // outputTree->Branch("eleIDMVATrg",             &treeLeaf.eleIDMVATrg);
+  // outputTree->Branch("eledEtaseedAtVtx",        &treeLeaf.eledEtaseedAtVtx);
+  // outputTree->Branch("eleE1x5",                 &treeLeaf.eleE1x5);
+  // outputTree->Branch("eleE2x5",                 &treeLeaf.eleE2x5);
+  // outputTree->Branch("eleE5x5",                 &treeLeaf.eleE5x5);
+  // outputTree->Branch("eleE1x5Full5x5",          &treeLeaf.eleE1x5Full5x5);
+  // outputTree->Branch("eleE2x5Full5x5",          &treeLeaf.eleE2x5Full5x5);
+  // outputTree->Branch("eleE5x5Full5x5",          &treeLeaf.eleE5x5Full5x5);
+  // outputTree->Branch("eleR9Full5x5",                &treeLeaf.eleR9Full5x5);
+  // outputTree->Branch("eleEcalDrivenSeed",           &treeLeaf.eleEcalDrivenSeed);
+  // outputTree->Branch("eleDr03EcalRecHitSumEt",      &treeLeaf.eleDr03EcalRecHitSumEt);
+  // outputTree->Branch("eleDr03HcalDepth1TowerSumEt", &treeLeaf.eleDr03HcalDepth1TowerSumEt);
+  // outputTree->Branch("eleDr03HcalDepth2TowerSumEt", &treeLeaf.eleDr03HcalDepth2TowerSumEt);
+  // outputTree->Branch("eleDr03HcalTowerSumEt",       &treeLeaf.eleDr03HcalTowerSumEt);
+  // outputTree->Branch("eleDr03TkSumPt",              &treeLeaf.eleDr03TkSumPt);
+  // outputTree->Branch("elecaloEnergy",               &treeLeaf.elecaloEnergy);
+  // outputTree->Branch("eleTrkdxy",                   &treeLeaf.eleTrkdxy);
+  // outputTree->Branch("eleKFHits",                   &treeLeaf.eleKFHits);
+  // outputTree->Branch("eleKFChi2",                   &treeLeaf.eleKFChi2);
+  // outputTree->Branch("eleGSFPt",                    &treeLeaf.eleGSFPt);
+  // outputTree->Branch("eleGSFEta",                   &treeLeaf.eleGSFEta);
+  // outputTree->Branch("eleGSFPhi",                   &treeLeaf.eleGSFPhi);
+  // outputTree->Branch("eleGSFCharge",                &treeLeaf.eleGSFCharge);
+  // outputTree->Branch("eleGSFHits",                  &treeLeaf.eleGSFHits);
+  // outputTree->Branch("eleGSFMissHits",              &treeLeaf.eleGSFMissHits);
+  // outputTree->Branch("eleGSFNHitsMax",              &treeLeaf.eleGSFNHitsMax);
+  // outputTree->Branch("eleGSFVtxProb",               &treeLeaf.eleGSFVtxProb);
+  // outputTree->Branch("eleGSFlxyPV",                 &treeLeaf.eleGSFlxyPV);
+  // outputTree->Branch("eleGSFlxyBS",                 &treeLeaf.eleGSFlxyBS);
+  // outputTree->Branch("eleBCEn",                     &treeLeaf.eleBCEn);
+  // outputTree->Branch("eleBCEta",                    &treeLeaf.eleBCEta);
+  // outputTree->Branch("eleBCPhi",                    &treeLeaf.eleBCPhi);
+  // outputTree->Branch("eleBCS25",                    &treeLeaf.eleBCS25);
+  // outputTree->Branch("eleBCS15",                    &treeLeaf.eleBCS15);
+  // outputTree->Branch("eleBCSieie",                  &treeLeaf.eleBCSieie);
+  // outputTree->Branch("eleBCSieip",                  &treeLeaf.eleBCSieip);
+  // outputTree->Branch("eleBCSipip",                  &treeLeaf.eleBCSipip);
+  outputTree->Branch("eleFiredTrgs",                &treeLeaf.eleFiredTrgs, "eleFiredTrgs/I");
 
+  //  if (runeleIDVID)
+  outputTree->Branch("eleIDbit",  &treeLeaf.eleIDbit, "eleIDbit/s");
+
+  //devel only
+    // outputTree->Branch("eleESEnP1Raw",              &treeLeaf.eleESEnP1Raw);
+    // outputTree->Branch("eleESEnP2Raw",              &treeLeaf.eleESEnP2Raw);
+    // outputTree->Branch("eleESEnEta",                &treeLeaf.eleESEnEta);
+    // outputTree->Branch("eleESEnPhi",                &treeLeaf.eleESEnPhi);
+    // outputTree->Branch("eleESEnZ",                  &treeLeaf.eleESEnZ);
+    // outputTree->Branch("eleESEnP",                  &treeLeaf.eleESEnP);
+    // outputTree->Branch("eleESEnX",                  &treeLeaf.eleESEnX);
+    // outputTree->Branch("eleESEnY",                  &treeLeaf.eleESEnY);
+    // outputTree->Branch("eleESEnS",                  &treeLeaf.eleESEnS);
+    // outputTree->Branch("eleESEnE",                  &treeLeaf.eleESEnE);
+    // outputTree->Branch("nGSFTrk",                   &treeLeaf.nGSFTrk);
+    // outputTree->Branch("gsfPt",                     &treeLeaf.gsfPt);
+    // outputTree->Branch("gsfEta",                    &treeLeaf.gsfEta);
+    // outputTree->Branch("gsfPhi",                    &treeLeaf.gsfPhi);
 }
--- Skimming/TInputOutputTree.h
+++ Skimming/TInputOutputTree.h
@@ -26,6 +26,7 @@ class TInputOutputTree{
   
   virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; } 
   struct InputTreeLeaves{
+    //global event
     Int_t     nVtx;		
     Int_t     run;		
     Long64_t  event;		
@@ -43,10 +44,109 @@ class TInputOutputTree{
     ULong64_t HLTEleMuXIsPrescaled;
     ULong64_t HLTPhoIsPrescaled;
     ULong64_t HLTJetIsPrescaled;
+    //electron
+    Int_t          nEle;
+    vector<int>    *eleCharge;
+    vector<int>    *eleChargeConsistent;
+    vector<float>  *eleEn;
+    vector<float>  *eleSCEn;
+    /* vector<float>  eleESEn; */
+    /* vector<float>  eleESEnP1; */
+    /* vector<float>  eleESEnP2; */
+    /* vector<float>  eleESEnP1Raw; */
+    /* vector<float>  eleESEnP2Raw; */
+    vector<float>  *eleD0;
+    vector<float>  *eleDz;
+    vector<float>  *elePt;
+    vector<float>  *eleEta;
+    vector<float>  *elePhi;
+    vector<float>  *eleR9;
+    vector<float>  *eleSCEta;
+    vector<float>  *eleSCPhi;
+    vector<float>  *eleSCRawEn;
+    /* vector<float>  eleSCEtaWidth; */
+    /* vector<float>  eleSCPhiWidth; */
+    vector<float>  *eleHoverE;
+    vector<float>  *eleEoverP;
+    /* vector<float>  eleEoverPout; */
+    /* vector<float>  eleEoverPInv; */
+    vector<float>  *eleBrem;
+    /* vector<float>  eledEtaAtVtx; */
+    /* vector<float>  eledPhiAtVtx; */
+    /* vector<float>  eledEtaAtCalo; */
+    vector<float>  *eleSigmaIEtaIEta;
+    vector<float>  *eleSigmaIEtaIPhi;
+    vector<float>  *eleSigmaIPhiIPhi;
+    vector<float>  *eleSigmaIEtaIEtaFull5x5;
+    vector<float>  *eleSigmaIPhiIPhiFull5x5;
+    vector<int>    *eleConvVeto;
+    vector<int>    *eleMissHits;
+    vector<float>  *eleESEffSigmaRR;
+    vector<float>  *elePFChIso;
+    vector<float>  *elePFPhoIso;
+    vector<float>  *elePFNeuIso;
+    vector<float>  *elePFPUIso;
+    vector<float>  *elePFClusEcalIso;
+    vector<float>  *elePFClusHcalIso;
+    /* vector<float>  eleIDMVANonTrg; */
+    /* vector<float>  eleIDMVATrg; */
+    /* vector<float>  eledEtaseedAtVtx; */
+    /* vector<float>  eleE1x5; */
+    /* vector<float>  eleE2x5; */
+    /* vector<float>  eleE5x5; */
+    /* vector<float>  eleE1x5Full5x5; */
+    /* vector<float>  eleE2x5Full5x5; */
+    /* vector<float>  eleE5x5Full5x5; */
+    /* vector<float>  eleR9Full5x5; */
+    /* vector<int>    eleEcalDrivenSeed; */
+    /* vector<float>  eleDr03EcalRecHitSumEt; */
+    /* vector<float>  eleDr03HcalDepth1TowerSumEt; */
+    /* vector<float>  eleDr03HcalDepth2TowerSumEt; */
+    /* vector<float>  eleDr03HcalTowerSumEt; */
+    /* vector<float>  eleDr03TkSumPt; */
+    /* vector<float>  elecaloEnergy; */
+    /* vector<float>  eleTrkdxy; */
+    /* vector<float>  eleKFHits; */
+    /* vector<float>  eleKFChi2; */
+    /* vector<float>  eleGSFChi2; */
+    /* vector<vector<float> > eleGSFPt; */
+    /* vector<vector<float> > eleGSFEta; */
+    /* vector<vector<float> > eleGSFPhi; */
+    /* vector<vector<float> > eleGSFCharge; */
+    /* vector<vector<int> >   eleGSFHits; */
+    /* vector<vector<int> >   eleGSFMissHits; */
+    /* vector<vector<int> >   eleGSFNHitsMax; */
+    /* vector<vector<float> > eleGSFVtxProb; */
+    /* vector<vector<float> > eleGSFlxyPV; */
+    /* vector<vector<float> > eleGSFlxyBS; */
+    /* vector<vector<float> > eleBCEn; */
+    /* vector<vector<float> > eleBCEta; */
+    /* vector<vector<float> > eleBCPhi; */
+    /* vector<vector<float> > eleBCS25; */
+    /* vector<vector<float> > eleBCS15; */
+    /* vector<vector<float> > eleBCSieie; */
+    /* vector<vector<float> > eleBCSieip; */
+    /* vector<vector<float> > eleBCSipip; */
+    /* vector<vector<float> > eleESEnEta; */
+    /* vector<vector<float> > eleESEnPhi; */
+    /* vector<vector<int> >   eleESEnZ; */
+    /* vector<vector<int> >   eleESEnP; */
+    /* vector<vector<int> >   eleESEnX; */
+    /* vector<vector<int> >   eleESEnY; */
+    /* vector<vector<int> >   eleESEnS; */
+    /* vector<vector<float> > eleESEnE; */
+    /* Int_t nGSFTrk; */
+    /* vector<float> gsfPt; */
+    /* vector<float> gsfEta; */
+    /* vector<float> gsfPhi; */
+    vector<int>    *eleFiredTrgs;
+    vector<UShort_t> *eleIDbit; 
   };
 
   
   InputTreeLeaves treeLeaf;
+
+  //Global branches
   TBranch  *b_nVtx; //!
   TBranch  *b_run;
   TBranch  *b_event;
@@ -64,6 +164,103 @@ class TInputOutputTree{
   TBranch  *b_HLTEleMuXIsPrescaled;
   TBranch  *b_HLTPhoIsPrescaled;
   TBranch  *b_HLTJetIsPrescaled;
+  //electron branches
+  TBranch  *b_nEle;		 
+  TBranch  *b_eleCharge;           // /I
+  TBranch  *b_eleChargeConsistent; // /I
+  TBranch  *b_eleEn;		 
+  TBranch  *b_eleSCEn;           
+  /* vector<float>  eleESEn; */
+  /* vector<float>  eleESEnP1; */
+  /* vector<float>  eleESEnP2; */
+  /* vector<float>  eleESEnP1Raw; */
+  /* vector<float>  eleESEnP2Raw; */
+ TBranch  *b_eleD0;
+ TBranch  *b_eleDz;
+ TBranch  *b_elePt;
+ TBranch  *b_eleEta;
+ TBranch  *b_elePhi;
+ TBranch  *b_eleR9;
+ TBranch  *b_eleSCEta;
+ TBranch  *b_eleSCPhi;
+ TBranch  *b_eleSCRawEn;
+  /* vector<float>  eleSCEtaWidth; */
+  /* vector<float>  eleSCPhiWidth; */
+ TBranch  *b_eleHoverE;
+ TBranch  *b_eleEoverP;
+  /* vector<float>  eleEoverPout; */
+  /* vector<float>  eleEoverPInv; */
+ TBranch  *b_eleBrem;
+  /* vector<float>  eledEtaAtVtx; */
+  /* vector<float>  eledPhiAtVtx; */
+  /* vector<float>  eledEtaAtCalo; */
+ TBranch  *b_eleSigmaIEtaIEta;
+ TBranch  *b_eleSigmaIEtaIPhi;
+ TBranch  *b_eleSigmaIPhiIPhi;
+ TBranch  *b_eleSigmaIEtaIEtaFull5x5;
+ TBranch  *b_eleSigmaIPhiIPhiFull5x5;
+ TBranch  *b_eleConvVeto;  // /I
+ TBranch  *b_eleMissHits; // /I
+ TBranch  *b_eleESEffSigmaRR;
+ TBranch  *b_elePFChIso;
+ TBranch  *b_elePFPhoIso;
+ TBranch  *b_elePFNeuIso;
+ TBranch  *b_elePFPUIso;
+ TBranch  *b_elePFClusEcalIso;
+ TBranch  *b_elePFClusHcalIso;
+  /* vector<float>  eleIDMVANonTrg; */
+  /* vector<float>  eleIDMVATrg; */
+  /* vector<float>  eledEtaseedAtVtx; */
+  /* vector<float>  eleE1x5; */
+  /* vector<float>  eleE2x5; */
+  /* vector<float>  eleE5x5; */
+  /* vector<float>  eleE1x5Full5x5; */
+  /* vector<float>  eleE2x5Full5x5; */
+  /* vector<float>  eleE5x5Full5x5; */
+  /* vector<float>  eleR9Full5x5; */
+  /* vector<int>    eleEcalDrivenSeed; */
+  /* vector<float>  eleDr03EcalRecHitSumEt; */
+  /* vector<float>  eleDr03HcalDepth1TowerSumEt; */
+  /* vector<float>  eleDr03HcalDepth2TowerSumEt; */
+  /* vector<float>  eleDr03HcalTowerSumEt; */
+  /* vector<float>  eleDr03TkSumPt; */
+  /* vector<float>  elecaloEnergy; */
+  /* vector<float>  eleTrkdxy; */
+  /* vector<float>  eleKFHits; */
+  /* vector<float>  eleKFChi2; */
+  /* vector<float>  eleGSFChi2; */
+  /* vector<vector<float> > eleGSFPt; */
+  /* vector<vector<float> > eleGSFEta; */
+  /* vector<vector<float> > eleGSFPhi; */
+  /* vector<vector<float> > eleGSFCharge; */
+  /* vector<vector<int> >   eleGSFHits; */
+  /* vector<vector<int> >   eleGSFMissHits; */
+  /* vector<vector<int> >   eleGSFNHitsMax; */
+  /* vector<vector<float> > eleGSFVtxProb; */
+  /* vector<vector<float> > eleGSFlxyPV; */
+  /* vector<vector<float> > eleGSFlxyBS; */
+  /* vector<vector<float> > eleBCEn; */
+  /* vector<vector<float> > eleBCEta; */
+  /* vector<vector<float> > eleBCPhi; */
+  /* vector<vector<float> > eleBCS25; */
+  /* vector<vector<float> > eleBCS15; */
+  /* vector<vector<float> > eleBCSieie; */
+  /* vector<vector<float> > eleBCSieip; */
+  /* vector<vector<float> > eleBCSipip; */
+  /* vector<vector<float> > eleESEnEta; */
+  /* vector<vector<float> > eleESEnPhi; */
+  /* vector<vector<int> >   eleESEnZ; */
+  /* vector<vector<int> >   eleESEnP; */
+  /* vector<vector<int> >   eleESEnX; */
+  /* vector<vector<int> >   eleESEnY; */
+  /* vector<vector<int> >   eleESEnS; */
+  /* vector<vector<float> > eleESEnE; */
+  /* Int_t nGSFTrk; */
+  /* vector<float> gsfPt; */
+  /* vector<float> gsfEta; */
+  /* vector<float> gsfPhi; */
+ TBranch  *b_eleFiredTrgs; // /I
+ TBranch  *b_eleIDbit;  // /U
 };
 
 
